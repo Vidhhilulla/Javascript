6a1140ef9cebfa64d28a9f0efe05f7a8d6fc960b
Vidhhilulla <lullavidhhi2011@gmail.com>
2023-08-20 10:24:16 +0530

    Completed setInterval and ClearInterval

---
+++ Learningjs/index.js
@@ -0,0 +1,9 @@
+console.log("hi")
+var a=25
+console.log(a)
+// Here,we didnt need to tell javascript that it is an integer
+let _=5
+console.log(_)
+// _ and dollar are the only special characters allowed
+
+console.log("8")
