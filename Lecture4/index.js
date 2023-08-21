65b1a9cf0d75cd33e7d2bb5a4373725fac5b52d5
Vidhhilulla <lullavidhhi2011@gmail.com>
2023-08-21 17:36:53 +0530

    started events

--- Lecture4/index.js
+++ Lecture4/index.js
@@ -19,7 +19,7 @@
 
 let arr=[1,2,3,4,5]
 
-// preference array const
+// preference array ko keep const
 let sumArr=(arr)=>
 {
     let sum=0
--- Lecture4/practice.js
+++ Lecture4/practice.js
@@ -1,2 +1,28 @@
-const btn=document.querySelector('#btn')
-console.log(btn.innerHTML)
+const button=document.querySelector('.clickbutton')
+const password=document.querySelector(".password")
+const input=document.querySelector('.input-value')
+
+button.addEventListener('click',(event)=>
+{
+    // console.log(button.textContent);
+    // console.log("CLICKED");
+    if(button.textContent==='show password')
+    {
+       input.innerHTML=password.value;
+       if(input.innerText==='')
+       button.textContent='show password'
+        else 
+       button.textContent='hide password' 
+
+    }
+    else if(button.textContent==='hide password')
+    {
+        input.innerHTML='';
+        button.textContent='show password'
+
+    }
+    
+})
+
+
+
